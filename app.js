906e247cc2e5550a344753ac3e51da2bd9292713
Shalommoise <mshalom689@gmail.com>
2020-08-28 12:44:45 +0100

    added cors to allow frontend to access api

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const apiRouter = require("./routers/api.router");
 const {
@@ -7,6 +8,7 @@ const {
   serverErrorHandler,
 } = require("./errors");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
