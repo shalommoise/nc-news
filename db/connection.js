3e85ecd5a8cadabb53f474ebee7360c27dd68ea0
Shalommoise <mshalom689@gmail.com>
2021-06-03 14:03:53 +0100

    config

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,23 @@
+
+const { DB_URL } = process.env;
 const ENV = process.env.NODE_ENV || "development";
 
 const test = 'nc_news_test';
 const dev = 'nc_news';
+ 
+
 const config = {
    user: "shalom",
   password: "pass",
   host: "localhost",
   port: 5432,
-  database: test
+  database: dev,
+    connection: {
+      connectionString: DB_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      }
+  }
 };
 
 const {Pool, Client} = require('pg');
