bf91de3e7bb6486eb48bbab5e7777656b775a169
shalommoise <shalommoise@github.com>
2021-07-15 17:12:56 +0100

    removed pool.connect()

--- models/topics.model.js
+++ models/topics.model.js
@@ -2,10 +2,7 @@ const{ pool }= require("../db/connection");
 
 exports.getTopics = () =>
 
-// return   pool.connect()
-  // .then(()=>
   pool.query("SELECT * FROM topics;")
-  // )
  .then((res) => {
    return res.rows
 })
@@ -14,11 +11,7 @@ exports.getTopics = () =>
 
 
 exports.getSingleTopic = (slug) =>
-// pool.connect()
-// .then(()=>
 pool.query(`SELECT * FROM topics WHERE slug = '${slug}';`)
-// )
-
 .then((res)=>{
 const [topic] = res.rows;
 return topic;
